f60e3372f12e8a206bb786e7b4d6d50cbceeea95
abing <252815825@qq.com>
2024-06-26 13:28:46 +0800

    个人页面前端判断非空

--- src/components/User/Userupdateinfo.vue
+++ src/components/User/Userupdateinfo.vue
@@ -1,43 +1,33 @@
 <template>
   <el-container class="home-container">
     <el-main>
-      <el-form :model="user" label-width="120px" class="user-form">
+      <el-form :model="user" :rules="rules" ref="userForm" label-width="120px" class="user-form">
         <el-row>
           <el-col :span="8">
-            <el-form-item label="用户名">
-              <el-input v-model="user.username"  :disabled = "true"></el-input>
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="user.username" :disabled="true"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item label="昵称">
+            <el-form-item label="昵称" prop="nickname">
               <el-input v-model="user.nickname"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item label="电子邮件">
+            <el-form-item label="电子邮件" prop="email">
               <el-input v-model="user.email"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="8">
-            <el-form-item label="科研机构">
+            <el-form-item label="科研机构" prop="organization">
               <el-input v-model="user.organization"></el-input>
             </el-form-item>
           </el-col>
-<!--          <el-col :span="8">-->
-<!--            <el-form-item label="注册时间">-->
-<!--              <el-input v-model="user.createTime"></el-input>-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
-<!--          <el-col :span="8">-->
-<!--            <el-form-item label="活跃度">-->
-<!--              <el-input v-model="user.userPic"></el-input>-->
-<!--            </el-form-item>-->
-<!--          </el-col>-->
         </el-row>
         <el-form-item>
-          <el-button type="primary" @click="updateinfo">提交</el-button>
+          <el-button type="primary" @click="submitForm">提交</el-button>
         </el-form-item>
       </el-form>
     </el-main>
@@ -56,40 +46,52 @@ export default {
         nickname: "",
         email: "",
         organization: ""
-
       },
+      rules: {
+        nickname: [
+          { required: true, message: '请输入昵称', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入电子邮件', trigger: 'blur' },
+          { type: 'email', message: '请输入有效的电子邮件地址', trigger: ['blur', 'change'] }
+        ],
+        organization: [
+          { required: true, message: '请输入科研机构', trigger: 'blur' }
+        ]
+      }
     };
   },
   async created() {
-
-    await this.getUserInformatonrmaton();
-
-
+    await this.getUserInformation();
   },
   methods: {
     submitForm() {
-      // 提交逻辑
-      console.log('Form submitted', this.user);
+      this.$refs.userForm.validate((valid) => {
+        if (valid) {
+          this.updateinfo();
+        } else {
+          console.log('表单验证失败');
+          return false;
+        }
+      });
     },
-    async getUserInformatonrmaton() {
-      const {data:infomation} = await axios.get("/api/user/userInfo");
-      this.user=infomation.data;
-      console.log("ceshi");
-      console.log(data);
-      console.log(user);
+    async getUserInformation() {
+      const { data: information } = await axios.get("/api/user/userInfo");
+      this.user = information.data;
     },
-    async updateinfo()
-    {
-      const {code: responses} = await axios.put("/api/user/update",this.user);
-
+    async updateinfo() {
+      const responses = await axios.put("/api/user/update", this.user);
       console.log("修改");
       console.log(this.user);
       console.log(responses);
-      this.$message.success('修改成功');
-    },
-    showUserInformatonrmaton() {
-      this.user=infomation.data;
-    },
+      if (responses.data.code == 0) {
+        this.$message.success('修改成功');
+        this.$router.go(-1);
+      }
+      else {
+        this.$message.error('修改失败');
+      }
+    }
   }
 };
 </script>
